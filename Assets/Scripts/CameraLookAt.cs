ea94965c622bbd1f581cad923bf3e65bdd15a4ec
Kyle Powers <kwpowers@gmail.com>
2021-06-09 03:40:14 -0400

    Added Sleeping Guard Cutscene Activation.

--- Assets/Scripts/CameraLookAt.cs
+++ Assets/Scripts/CameraLookAt.cs
@@ -6,6 +6,14 @@ public class CameraLookAt : MonoBehaviour
 {
     [SerializeField]
     private Transform _target = null;
+    [SerializeField]
+    private Transform _startingView = null;
+
+    private void Start()
+    {
+        transform.position = _startingView.position;
+        transform.rotation = _startingView.rotation;
+    }
 
     void Update()
     {
---
+++ Assets/Scripts/GrabKeyCardActivation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class GrabKeyCardActivation : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _cutsceneObj = null;
+    private bool _hasPlayed = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && _hasPlayed == false)
+        {
+            if (_cutsceneObj != null)
+            {
+                _hasPlayed = true;
+                _cutsceneObj.SetActive(true);
+            }
+        }
+    }
+}
